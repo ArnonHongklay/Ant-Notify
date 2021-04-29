d1d724ef56e9876542c81ab6567a0c8bb1ada2cb
Arnon Hongklay <arnon@hongklay.com>
2021-04-29 09:17:27 +0700

    Add merge

--- app/src/main/java/com/example/paymentdetection/ReceiveSms.java
+++ app/src/main/java/com/example/paymentdetection/ReceiveSms.java
@@ -5,66 +5,123 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class ReceiveSms extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 //        Toast.makeText(context, "SMS RECEVIED!", Toast.LENGTH_LONG).show();
         if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
-            Bundle bundle = intent.getExtras();
-            SmsMessage[] msgs = null;
-            String msgFrom;
-
-            RequestQueue queue = Volley.newRequestQueue(context);
-            String url ="https://www.google.com";
-
-            if(bundle != null) {
-                try {
-                    Object[] pdus = (Object[]) bundle.get("pdus");
-                    msgs = new SmsMessage[pdus.length];
-
-                    for(int i = 0; i < msgs.length; i++) {
-                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                        msgFrom = msgs[i].getOriginatingAddress();
-                        String msgBody = msgs[i].getMessageBody();
-
-                        // Request a string response from the provided URL.
-                        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                                new Response.Listener<String>() {
-                                    @Override
-                                    public void onResponse(String response) {
-                                        // Display the first 500 characters of the response string.
-//                            textView.setText("Response is: "+ response.substring(0,500));
 
 
-                                    }
-                                }, new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-//                    textView.setText("That didn't work!");
-                            }
-                        });
+            RequestQueue requestQueue = Volley.newRequestQueue(context);
+            String URL ="http://192.168.1.45/item/add";
+            String requestBody = "";
 
-                        Toast.makeText(context, "FROM: " + msgFrom + ", Body: " + msgBody, Toast.LENGTH_SHORT).show();
-// Add the request to the RequestQueue.
-                        queue.add(stringRequest);
+//            Bundle bundle = intent.getExtras();
+//            SmsMessage[] msgs = null;
+//            String msgFrom;
+//            if(bundle != null) {
+//                try {
+//                    Object[] pdus = (Object[]) bundle.get("pdus");
+//                    msgs = new SmsMessage[pdus.length];
+//
+//                    for(int i = 0; i < msgs.length; i++) {
+//                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+//                        msgFrom = msgs[i].getOriginatingAddress();
+//                        String msgBody = msgs[i].getMessageBody();
+//                    }
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
 
-
-                    }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
                 }
-            }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        JSONObject jsonBody = new JSONObject();
+
+                        jsonBody.put("name", "Android Volley Demo");
+
+                        Log.i("VOLLEY", jsonBody.toString());
+
+                        return jsonBody.toString() == null ? null : jsonBody.toString().getBytes("utf-8");
+
+                    } catch (UnsupportedEncodingException | JSONException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+
+
+//            // Request a string response from the provided URL.
+//            StringRequest stringRequest = new StringRequest(Request.Method.POST, url + "/item/add",
+//                    new Response.Listener<String>() {
+//
+//                        @Override
+//                        public void onResponse(String response) {
+//                            // Display the first 500 characters of the response string.
+//                            Log.e(TAG, "Response is: "+ response);
+//                        }
+//                    }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    Log.e(TAG, "That didn't work!" + error);
+//                }
+//            });
+//
+//            Toast.makeText(context, "FROM: " + msgFrom + ", Body: " + msgBody, Toast.LENGTH_SHORT).show();
+//// Add the request to the RequestQueue.
+//            queue.add(stringRequest);
+
+
         }
     }
-//    public void onSend(Context context, Intent intent) {}
 }
